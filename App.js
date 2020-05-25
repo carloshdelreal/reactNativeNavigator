6af94310b302067967a8fe11ec8fe4f8b2d3502a
Carlos Del Real <carloshdelreal@gmail.com>
2020-05-25 16:44:47 -0500

    fix dependencies and packages

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
-import Bug from './solutions/1';
+import Bug from './bugs/4';
 
 export default class App extends React.Component {
   render() {
